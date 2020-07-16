bc9d7c9660122e23018e9ec25dedbbcab2661924
Dusan Jonjin <dusan.jonjin@gmail.com>
2020-07-16 20:15:04 +0200

    Function with rules for numeric buttons done.

--- src/Components/CalculatorApp.jsx
+++ src/Components/CalculatorApp.jsx
@@ -5,13 +5,44 @@ import { Display } from './Display/Display';
 export function CalculatorApp() {
 
     const [computation, setComputation] = useState('0');
+
     const [result, setResult] = useState('0');
+
+
+    const operators = ['+', '-', 'x', '÷'];
+
+    const delFirstCharacter = char => {
+        const delFirst = computation.slice(0, 0);
+        setComputation(delFirst + char);
+    }
+    
+
+    const handleNumBtnClick = number => {
+        const zeroOperators = ['+0', '-0', 'x0', '÷0'];
+        const lastTwoCharacters = computation.slice(-2);
+        // Max size number, because of math.round accuracy issue (rounded on 14 digits):
+        if (computation.length >= 14) {
+            if (!operators.some(operator => computation.includes(operator))) {
+                return;
+            }
+            else if (computation.length >= 19) return;
+        }
+        if (computation.length === 1 && computation === '0') {
+            delFirstCharacter(number);
+        }
+        else if (zeroOperators.includes(lastTwoCharacters)) {
+            return;
+        }
+        else 
+            setComputation(computation + number);
+            setResult('0');
+    }
 
     return (
         <section id='calculator'>
             <Display computation={computation}
                      result={result} />
-            <Keypad />
+            <Keypad numBtnClick={handleNumBtnClick} />
         </section>
     );
 }
