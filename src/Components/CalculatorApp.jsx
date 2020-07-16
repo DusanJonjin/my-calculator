e879446cd45c4e34eee67f5c1412d0d7b04df802
Dusan Jonjin <dusan.jonjin@gmail.com>
2020-07-16 20:33:50 +0200

    Declared function with rules for operator buttons.

--- src/Components/CalculatorApp.jsx
+++ src/Components/CalculatorApp.jsx
@@ -8,8 +8,16 @@ export function CalculatorApp() {
 
     const [result, setResult] = useState('0');
 
+    const [decimalAllowed, setDecimalAllowed] = useState(true);
+
 
     const operators = ['+', '-', 'x', '÷'];
+
+    const lastCharacter = computation.slice(-1);
+
+    const secToLastCharacter = computation.charAt(computation.length - 2);
+
+    const delLastCharacter = computation.slice(0, -1);
 
     const delFirstCharacter = char => {
         const delFirst = computation.slice(0, 0);
@@ -38,11 +46,59 @@ export function CalculatorApp() {
             setResult('0');
     }
 
+
+    const handleOperatorBtnClick = operator => {
+        if (computation.length >= 19) {
+            return;
+        }
+
+        if (computation.length === 1 && computation === '0') {
+            if (operator === '-') {
+                delFirstCharacter(operator);
+                setResult('0');
+            }
+            else return;
+        }
+
+        else if (operators.includes(lastCharacter)) {
+            if (computation.length <= 1) {
+                return;
+            }
+            else if (computation.length > 1 && operator !== '-' && !operators.includes(secToLastCharacter)) {
+                setComputation(delLastCharacter + operator);
+                setResult('0');
+            }
+            else if (operators.includes(secToLastCharacter)) {
+                return;
+            }
+           else if (operator === '-' && operator === lastCharacter) {
+                return;
+            }
+           else {
+               setComputation(computation + operator);
+               setResult('0')
+           }
+        }
+
+        else if (lastCharacter === '.') {
+            setComputation(computation + '0' + operator)
+            setDecimalAllowed(true);
+            setResult('0')
+        }
+
+        else {
+            setComputation(computation + operator); 
+            setDecimalAllowed(true);
+            setResult('0');
+        }
+    }
+
     return (
         <section id='calculator'>
             <Display computation={computation}
                      result={result} />
-            <Keypad numBtnClick={handleNumBtnClick} />
+            <Keypad numBtnClick={handleNumBtnClick}
+                    operatorBtnClick={handleOperatorBtnClick} />
         </section>
     );
 }
