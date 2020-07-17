5730c99190b3567f791feb1f994efff9cd077e30
Dusan Jonjin <dusan.jonjin@gmail.com>
2020-07-17 11:52:31 +0200

    Change font size for result, when it's too long.

--- src/Components/Display/ResultDisplay.jsx
+++ src/Components/Display/ResultDisplay.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 export function ResultDisplay({ result }) {
 
+    const newClass = length => {
+        if (length > 14) {
+            if (length > 18) {
+                if (length > 22) {
+                    return ` very_small`;
+                }  
+                return ` small`;    
+            }
+            return ` medium`;
+        }
+        else return ``;       
+    }
+
     return (
-        <div className='result-display'>
+        <div className={`result-display${newClass(result.length)}`}>
             {result}
         </div>
+
     );
 }
